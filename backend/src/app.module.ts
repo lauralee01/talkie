472aba22d9df1fa2cad8bb46572187f2328a3c08
lauralee01 <lauralee285@yahoo.com>
2026-09-15 14:43:38 -0400

    feat(backend): initializing prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CallsModule } from './calls/calls.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     CallsModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
