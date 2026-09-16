4608a289f7a776a6a8644fbc6029e08f96e7c407
lauralee01 <lauralee285@yahoo.com>
2026-09-16 15:26:04 -0400

    feat(backend): update prisma service

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+    constructor() {
+        const connectionString = process.env.DATABASE_URL;
+
+        if (!connectionString) {
+            throw new Error('DATABASE_URL is not configured');
+        }
+
+        const adapter = new PrismaPg({
+            connectionString,
+        });
+
+        super({ adapter });
+    }
+}
