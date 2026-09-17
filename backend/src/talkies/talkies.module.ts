b0dc32f584ae13c0053f503ae32e7f7afd1fdb2e
lauralee01 <lauralee285@yahoo.com>
2026-09-17 11:53:59 -0400

    feat(backend): created controller for fetching all talkies

--- backend/src/talkies/talkies.module.ts
+++ backend/src/talkies/talkies.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { TalkiesService } from './talkies.service';
+import { TalkiesController } from './talkies.controller';
 
 @Module({
   imports: [PrismaModule],
   providers: [TalkiesService],
   exports: [TalkiesService],
+  controllers: [TalkiesController],
 })
 export class TalkiesModule { }
